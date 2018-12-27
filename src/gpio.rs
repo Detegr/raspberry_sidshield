73ab4c0bade963c13f92508a3e0d1be388d681ad
Antti Keränen <detegr@gmail.com>
2018-12-27 13:31:06 +0200

    Command line flags. Allow running without GPIO hardware

--- src/gpio.rs
+++ src/gpio.rs
@@ -2,6 +2,26 @@ use rppal::gpio::{Gpio, Level, Mode};
 use std::thread;
 use std::time::Duration;
 
+pub trait SidGpio {
+    fn write(&mut self, pin: u8, level: Level);
+    fn set_mode(&mut self, pin: u8, mode: Mode);
+}
+
+impl SidGpio for Gpio {
+    fn write(&mut self, pin: u8, level: Level) {
+        Gpio::write(self, pin, level);
+    }
+    fn set_mode(&mut self, pin: u8, mode: Mode) {
+        Gpio::set_mode(self, pin, mode);
+    }
+}
+
+struct DummyGpio;
+impl SidGpio for DummyGpio {
+    fn write(&mut self, _pin: u8, _level: Level) {}
+    fn set_mode(&mut self, _pin: u8, _mode: Mode) {}
+}
+
 const ADDR_DATA: u8 = 10;
 const DATA: u8 = 24;
 const SHIFT_CLOCK: u8 = 16;
@@ -20,7 +40,7 @@ fn get_level_for_bit(value: u8, bit: u8) -> Level {
     }
 }
 
-pub fn output_to_gpio(gpio: &mut Gpio, addr: u8, data: u8) {
+pub fn output_to_gpio(gpio: &mut dyn SidGpio, addr: u8, data: u8) {
     for i in (0..8).rev() {
         let addr_level = get_level_for_bit(addr, i);
         let data_level = get_level_for_bit(data, i);
@@ -39,13 +59,13 @@ pub fn output_to_gpio(gpio: &mut Gpio, addr: u8, data: u8) {
     output_to_6581(gpio);
 }
 
-fn output_to_6581(gpio: &mut Gpio) {
+fn output_to_6581(gpio: &mut dyn SidGpio) {
     gpio.write(SID_CS, Level::Low);
     thread::sleep(Duration::from_micros(2));
     gpio.write(SID_CS, Level::High);
 }
 
-fn reset_6581(gpio: &mut Gpio) {
+fn reset_6581(gpio: &mut dyn SidGpio) {
     gpio.write(SID_RESET, Level::Low);
     thread::sleep(Duration::from_millis(1));
     gpio.write(SID_RESET, Level::High);
@@ -54,8 +74,17 @@ fn reset_6581(gpio: &mut Gpio) {
     gpio.write(SID_CS, Level::High);
 }
 
-pub fn init_gpio() -> rppal::gpio::Result<Gpio> {
-    let mut gpio = Gpio::new()?;
+pub fn init_gpio(disable_gpio: bool) -> rppal::gpio::Result<Box<dyn SidGpio>> {
+    let mut gpio: Box<dyn SidGpio> = match Gpio::new() {
+        Ok(gpio) => Box::new(gpio),
+        Err(e) => {
+            if disable_gpio {
+                Box::new(DummyGpio)
+            } else {
+                return Err(e);
+            }
+        }
+    };
 
     // Set up output pins
     gpio.set_mode(ADDR_DATA, Mode::Output);
@@ -67,7 +96,7 @@ pub fn init_gpio() -> rppal::gpio::Result<Gpio> {
     gpio.set_mode(SID_CS, Mode::Output);
     gpio.set_mode(SID_RESET, Mode::Output);
 
-    reset_6581(&mut gpio);
+    reset_6581(&mut *gpio);
 
     gpio.write(SHIFT_MR, Level::Low);
     gpio.write(SHIFT_CLOCK, Level::Low);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::{App, Arg, SubCommand};
 use rppal::pwm;
 use std::error::Error;
 use std::io;
@@ -7,15 +8,33 @@ mod gpio;
 
 const FRAME_ENDING_BYTE: u8 = 0xFF;
 
-fn main() -> rppal::gpio::Result<()> {
+fn main() {
+    let matches = App::new("Raspberry Pi SID shield")
+        .arg(
+            Arg::with_name("Enable debug output")
+                .short("d")
+                .long("debug"),
+        )
+        .arg(
+            Arg::with_name("Disable GPIO")
+                .short("g")
+                .long("disable_gpio"),
+        )
+        .get_matches();
+    let debug = matches.is_present("Enable debug output");
+    let disable_gpio = matches.is_present("Disable GPIO");
     let mut stdin = BufReader::new(io::stdin());
     let mut buf = Vec::with_capacity(64);
-    let mut gpio = gpio::init_gpio()?;
+    let mut gpio = gpio::init_gpio(disable_gpio).expect("Failed to initialize GPIO");
 
     // 1mhz clock with 50% duty cycle for 6581
-    let _pwm =
-        pwm::Pwm::with_frequency(pwm::Channel::Pwm0, 1000.0, 0.5, pwm::Polarity::Normal, true)
-            .expect("Failed to set PWM on pin 18");
+    let pwm =
+        pwm::Pwm::with_frequency(pwm::Channel::Pwm0, 1000.0, 0.5, pwm::Polarity::Normal, true);
+
+    if pwm.is_err() && !disable_gpio {
+        // Error out if gpio is not disabled and PWM setup failed
+        let _ = pwm.expect("Failed to set PWM on pin 18");
+    }
 
     loop {
         if let Err(e) = stdin.read_until(FRAME_ENDING_BYTE, &mut buf) {
@@ -33,11 +52,14 @@ fn main() -> rppal::gpio::Result<()> {
                 let data = chunk[1];
                 assert!(addr < 0x20); // Address is only 5 bits
 
-                gpio::output_to_gpio(&mut gpio, addr, data);
+                gpio::output_to_gpio(&mut *gpio, addr, data);
             }
         }
 
-        //println!("Frame: {:?}", buf);
+        if debug {
+            println!("Frame: {:?}", buf);
+        }
+
         buf.clear();
     }
 }
